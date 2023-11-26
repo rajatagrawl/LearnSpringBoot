d63db9dd3a5f18aca749e1ba4f7a43d5faa9e5ba
rajatagrawl <rajat1611@gmail.com>
2023-11-26 09:28:44 +0530

    Initialize Springboot project

---
+++ src/main/java/com/rajat3/rajat3/Rajat3Application.java
@@ -0,0 +1,13 @@
+package com.rajat3.rajat3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rajat3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rajat3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajat3/rajat3/Rajat3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat3.rajat3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rajat3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
